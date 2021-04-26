3b84eeb77789ce32a6bce583c3dc86e9c03bac05
Dgryzhkov <dgryzhkov@gmail.com>
2021-04-26 10:33:07 +0700

    fixes bug

--- src/main/kotlin/comission.kt
+++ src/main/kotlin/comission.kt
@@ -1,22 +1,35 @@
 const val COMISSION_PERCENT_MASTERCAR_MAESTRO = 0.006
 const val COMISSION_PRECENT_VISA_WORLD = 0.0075
-const val FIXED_COMISSION_MASTER_MAESTRO = 20.0
-const val FIXED_COMISSION_VISA_WORLD = 35.0
-const val NO_COMISSION_MAX_MASTERCARD_MAESTRO = 75000.00
+const val FIXED_COMISSION_MASTER_MAESTRO = 20*RUB_COP
+const val FIXED_COMISSION_VISA_WORLD = 35*RUB_COP
+const val NO_COMISSION_MAX_MASTERCARD_MAESTRO = 75_000*RUB_COP
 
-fun comission(sumTransfer: Int, limTranfer: Int, typeCard: String): Int {
-    val com = when (typeCard) {
+
+fun comission(sumTransfer: Int, previousTransfer: Int, typeCard: String): Int {
+    return when (typeCard) {
         MAESTRO, MASTER_CARD ->
-            if (NO_COMISSION_MAX_MASTERCARD_MAESTRO > sumTransfer + limTranfer) 0
-            else
-                (sumTransfer - limTranfer) * COMISSION_PERCENT_MASTERCAR_MAESTRO + FIXED_COMISSION_MASTER_MAESTRO
+            if (NO_COMISSION_MAX_MASTERCARD_MAESTRO > (sumTransfer + previousTransfer))
+                0
+            else ((((sumTransfer + previousTransfer) - NO_COMISSION_MAX_MASTERCARD_MAESTRO)
+                    * COMISSION_PERCENT_MASTERCAR_MAESTRO
+                    + FIXED_COMISSION_MASTER_MAESTRO) / RUB_COP).toInt()
 
-        VISA, WORLD -> if (FIXED_COMISSION_VISA_WORLD > sumTransfer * COMISSION_PRECENT_VISA_WORLD)
+        VISA, WORLD -> if (FIXED_COMISSION_VISA_WORLD*RUB_COP > ((sumTransfer * COMISSION_PRECENT_VISA_WORLD) / RUB_COP))
             FIXED_COMISSION_VISA_WORLD
-        else sumTransfer * COMISSION_PRECENT_VISA_WORLD
+        else ((sumTransfer * COMISSION_PRECENT_VISA_WORLD) / RUB_COP).toInt()
 
         else -> 0
     }
-    return com.toInt()
 }
 
+
+//((sumTransfer+previousTransfer)-NO_COMISSION_MAX_MASTERCARD_MAESTRO)*COMISSION_PERCENT_MASTERCAR_MAESTRO)
+//   +FIXED_COMISSION_MASTER_MAESTRO
+
+/*  MAX_MONEY_TRANSFER_VKPAY.toInt() ->
+  println("Первышены суммы перевода ${(MAX_MONEY_TRANSFER_VKPAY / RUB_COP).toInt()} рублей")
+  MAX_MONEY_TRANSFER_ALL_CARD_EXCEPT_VK.toInt() ->
+  println("Первышены суммы перевода ${(MAX_MONEY_TRANSFER_ALL_CARD_EXCEPT_VK / RUB_COP).toInt()} рублей")
+  else -> println(" комиссия составляет ${(comision / RUB_COP)} рублей")
+}
+*/
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,18 +4,15 @@ const val VISA = "VISA"
 const val WORLD = "Мир"
 const val VK_PAY = "VK Pay"
 const val RUB_COP = 100 // для перевода копеек в рубли
+//const val LIMIT_TRANSFER = 600_000*RUB_COP
+//const val LIMIT_VKPAY_TRANSFER =40_000*RUB_COP
+
 
 fun main() {
     val previousTransfer = 0
-    val moneyTransfer =RUB_COP* 80_00 // введи сумму перевода в рублях
-    val card_type = VK_PAY
-    val comision = limit(moneyTransfer, previousTransfer, card_type)
+    val moneyTransfer =RUB_COP* 80000 // введи сумму перевода в рублях
+    val card_type = MASTER_CARD
 
-    when (comision) {
-        MAX_MONEY_TRANSFER_VKPAY.toInt() ->
-            println("Первышены суммы перевода ${(MAX_MONEY_TRANSFER_VKPAY / RUB_COP).toInt()} рублей")
-        MAX_MONEY_TRANSFER_ALL_CARD_EXCEPT_VK.toInt() ->
-            println("Первышены суммы перевода ${(MAX_MONEY_TRANSFER_ALL_CARD_EXCEPT_VK / RUB_COP).toInt()} рублей")
-        else -> println(" комиссия составляет ${(comision / RUB_COP)} рублей")
-    }
+
+println("Комиссия составлет ${comission(moneyTransfer, previousTransfer,card_type)} рублей")
 }
